8f56404ea4d92763322fb88304ff2916c3e9a1e1
Brett Mainers <brettmainers@gmail.com>
2025-03-18 09:05:03 -0700

    Updated Main.java and Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Graph {
 
@@ -23,12 +26,48 @@ public class Graph {
     }
 
     public void print() {
-        for (String keys : adjList.keySet()) {
-            System.out.print(keys + ": ");
-            for (String string : adjList.get(keys)) {
+        for (String key : adjList.keySet()) {
+            System.out.print(key + ": ");
+            for (String string : adjList.get(key)) {
                 System.out.print(string + " -> " );
             }
             System.out.println();
+        }
+    }
+
+    public void breadthFirstSearch(String source) {
+        Queue<String> queue = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+
+        queue.offer(source);
+        visited.add(source);
+
+        while (!queue.isEmpty()) {
+            source = queue.poll();
+            System.out.println(source + " = visited");
+            for (int i = 0; i < adjList.get(source).size(); i++) {
+               if (!visited.contains(adjList.get(source).get(i))) 
+                queue.offer(adjList.get(source).get(i));
+                visited.add(adjList.get(source).get(i));
+            }
+        }
+    }
+
+    public void depthFirstSearch(String source) {
+        HashSet<String> visited = new HashSet<>();
+        dFSHelper(source, visited);
+    }
+
+    public void dFSHelper(String source, HashSet<String> visited) {
+        if (visited.contains(source)) {
+            return;
+        } else {
+            visited.add(source);
+            System.out.println(source + " = visited");
+        }
+
+        for (int i = 0; i < adjList.get(source).size(); i++) {
+                dFSHelper(adjList.get(source).get(i), visited);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,26 @@ public class Main {
         graph.addVertex("B");
         graph.addVertex("C");
         graph.addVertex("D");
+        graph.addVertex("E");
 
         graph.addEdge("A", "B");
+        graph.addEdge("B", "A");
         graph.addEdge("A", "C");
-        graph.addEdge("B", "C");
+        graph.addEdge("C", "A");
+        graph.addEdge("B", "D");
+        graph.addEdge("D", "B");
         graph.addEdge("C", "D");
+        graph.addEdge("D", "C");
+        graph.addEdge("D", "E");
+        graph.addEdge("E", "D");
 
         graph.print();
+        System.out.println("\nBreadth First Traversal:\n");
+        graph.breadthFirstSearch("E");
+        System.out.println("\nDepth First Traversal:\n");
+        graph.depthFirstSearch("E");
 
+        
 
 
     }
